8802e6fda02fab5ebaaa72dd37d56126526d6dcb
Paulo Gouveia <paulojlgouveia@gmail.com>
2017-11-22 09:56:59 +0000

    makefile to clean files; axelrod demo

--- part_II/srs/main.py
+++ part_II/srs/main.py
@@ -1,6 +1,7 @@
 
 import sys
-
+import axelrod as axl
+import _tkinter
 
 
 def main():
@@ -9,6 +10,17 @@ def main():
 	if sys.version_info < (3, 0):
 		sys.stdout.write("DENIED: requires Python 3.x\n")
 		sys.exit(1)
+	
+	
+	axl.seed(0)  # Set a seed
+	
+	players = [s() for s in axl.demo_strategies]  # Create players
+	
+	tournament = axl.Tournament(players)  # Create a tournament
+	
+	results = tournament.play()  # Play the tournament
+	
+	print(results.ranked_names)
 	
 	
 
