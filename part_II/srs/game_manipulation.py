e2f5e47f88039def9278255329e4e36d1824c4c1
Paulo Gouveia <paulojlgouveia@gmail.com>
2017-11-22 16:11:17 +0000

    cleanup on code and printing

--- part_II/srs/game_manipulation.py
+++ part_II/srs/game_manipulation.py
@@ -1,75 +1,78 @@
+
+from pprint import pprint
 import axelrod as axl
-import pprint
 
 
 def visualise_boxplot(tours):
-    result_item = [item[1] for item in tours]
-    for results in result_item:
-        plot = axl.Plot(results)
-        p = plot.boxplot()
-        p.show()
-        input()
+	result_item = [item[1] for item in tours]
+	for results in result_item:
+		plot = axl.Plot(results)
+		p = plot.boxplot()
+		p.show()
+		input()
 
 
 def visualise_win_distribution(tours):
-    result_item = [item[1] for item in tours]
-    for results in result_item:
-        plot = axl.Plot(results)
-        p = plot.winplot()
-        p.show()
-        input()
+	result_item = [item[1] for item in tours]
+	for results in result_item:
+		plot = axl.Plot(results)
+		p = plot.winplot()
+		p.show()
+		input()
 
 
 def visualise_payoff_matrix(tours):
-    result_item = [item[1] for item in tours]
-    for results in result_item:
-        plot = axl.Plot(results)
-        p = plot.payoff()
-        p.show()
-        input()
+	result_item = [item[1] for item in tours]
+	for results in result_item:
+		plot = axl.Plot(results)
+		p = plot.payoff()
+		p.show()
+		input()
 
 
 def visualise_tournaments_game(tours):
-    visualise_boxplot(tours)
-    visualise_win_distribution(tours)
-    visualise_payoff_matrix(tours)
-    input()
+	visualise_boxplot(tours)
+	visualise_win_distribution(tours)
+	visualise_payoff_matrix(tours)
+	input()
 
 
 def summary_results(tours):
-    result_item = [item[1] for item in tours]
-    for results in result_item:
-        summy = results.summarise()
-        pprint.pprint(summy)
+	result_item = [item[1] for item in tours]
+	for results in result_item:
+		summy = results.summarise()
+		pprint(summy)
 
 
 def interaction_results(tours):
-    tournaments = [item[0] for item in tours]
-    result_item = [item[1] for item in tours]
-    for x in range(0, len(result_item) - 1):
-        for index_pair, interaction in sorted(result_item[x].interactions.items()):
-            player1 = tournaments[x].players[index_pair[0]]
-            player2 = tournaments[x].players[index_pair[1]]
-            print('%s vs %s: %s' % (player1, player2, interaction[0]))
+	tournaments = [item[0] for item in tours]
+	result_item = [item[1] for item in tours]
+	for x in range(0, len(result_item) - 1):
+		for index_pair, interaction in sorted(result_item[x].interactions.items()):
+			player1 = tournaments[x].players[index_pair[0]]
+			player2 = tournaments[x].players[index_pair[1]]
+			print('%s vs %s: %s' % (player1, player2, interaction[0]))
 
 
 def view_winners(tours):
-    tournaments = [item[0] for item in tours]
-    result_item = [item[1] for item in tours]
-    for x in range(0, len(result_item) - 1):
-        matches = []
-        for index_pair, interaction in sorted(result_item[x].interactions.items()):
-            player1 = tournaments[x].players[index_pair[0]]
-            player2 = tournaments[x].players[index_pair[1]]
-            match = axl.Match([player1, player2])
-            match.result = interaction[0]
-            matches.append(match)
-        for match in matches:
-            print("{} v {}, winner: {}".format(match.players[0], match.players[1], match.winner()))
+	tournaments = [item[0] for item in tours]
+	result_item = [item[1] for item in tours]
+	for x in range(0, len(result_item) - 1):
+		matches = []
+		for index_pair, interaction in sorted(result_item[x].interactions.items()):
+			player1 = tournaments[x].players[index_pair[0]]
+			player2 = tournaments[x].players[index_pair[1]]
+			match = axl.Match([player1, player2])
+			match.result = interaction[0]
+			matches.append(match)
+		for match in matches:
+			print("{} v {}, winner: {}".format(match.players[0], match.players[1], match.winner()))
 
 
 def human_interation(players, n):
-    me = axl.Human(name='me')
-    player_including_me = [players[n], me]
-    match = axl.Match(player_including_me, turns=5)
-    match.play()
+	me = axl.Human(name='me')
+	player_including_me = [players[n], me]
+	match = axl.Match(player_including_me, turns=5)
+	match.play()
+	
+	
--- part_II/srs/main.py
+++ part_II/srs/main.py
@@ -1,41 +1,51 @@
-from aux_main import *
+
 import sys
+
+from aux_main import *
 
 
 def main():
-    # check if running under python3
-    if sys.version_info < (3, 0):
-        sys.stdout.write("DENIED: requires Python 3.x\n")
-        sys.exit(1)
+	# check if running under python3
+	if sys.version_info < (3, 0):
+		sys.stdout.write("DENIED: requires Python 3.x\n")
+		sys.exit(1)
 
-    players, tours = init()
+	players, tours = init()
 
-    # interactive mode (no flags)
-    main_modes = ["Visualising results",
-                  "Summarising tournament results",
-                  "Accessing the interactions",
-                  "Human interation"]
+	# interactive mode (no flags)
+	main_modes = ["View results",
+				"Summarise tournament results",
+				"Access the interactions",
+				"Human interation"]
 
-    while True:
-        print("\nexecution mode:")
-        print_numbered_list(main_modes)
-        cmd = input("\n-> ")
-        if cmd == "0":
-            visualise_mode_init(tours)
-        elif cmd == "1":
-            summary_results(tours)
-        elif cmd == "2":
-            accessing_mode_init(tours)
-        elif cmd == "3":
-            human_interation_init(players)
-        elif cmd == "q" or cmd == "quit":
-            break
-        elif cmd == "e" or cmd == "exit":
-            sys.exit(1)
-        else:
-            continue
-        print("queue the XP shutdown theme.\n")
+	while True:
+		print("\nexecution mode:")
+		print_numbered_list(main_modes)
+		cmd = input("\n-> ")
+		if cmd == "0":
+			visualise_mode_init(tours)
+			
+		elif cmd == "1":
+			summary_results(tours)
+			
+		elif cmd == "2":
+			accessing_mode_init(tours)
+			
+		elif cmd == "3":
+			human_interation_init(players)
+			
+		elif cmd == "q" or cmd == "quit":
+			break
+		
+		elif cmd == "e" or cmd == "exit":
+			sys.exit(0)
+			
+		else:
+			continue
+	
+	
+	print("queue the XP shutdown theme.\n")
 
 
 if __name__ == '__main__':
-    main()
+	main()
