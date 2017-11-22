d39233bb8c8e6233547444b0609250acea8600d1
Paulo Gouveia <paulojlgouveia@gmail.com>
2017-11-22 11:59:18 +0000

    ordered references

--- part_II/srs/main.py
+++ part_II/srs/main.py
@@ -13,11 +13,8 @@ def main():
 	
 	
 	axl.seed(0)  # Set a seed
-	
-	players = [s() for s in axl.demo_strategies]  # Create players
-	
+	players = [s() for s in axl.all_strategies]  # Create players
 	tournament = axl.Tournament(players)  # Create a tournament
-	
 	results = tournament.play()  # Play the tournament
 	
 	print(results.ranked_names)
